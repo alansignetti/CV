4a6801d9a02f66cca8a65fc6e413033bf7256b71
asignetti <asignetti@expand.com.uy>
2023-08-20 13:51:29 +1000

    Modified Location and Phone number

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,8 +257,8 @@
     new Typed(".typed", {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 25,
+      backSpeed: 25,
       backDelay: 2000,
     });
   }
